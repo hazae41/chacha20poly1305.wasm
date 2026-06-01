dbbd93aea5ef66bccb0c2f4d1da68b8ba72b5ca5
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-06-01 12:39:13 +0700

    f

--- src/node/mods/mod.test.ts
+++ src/node/mods/mod.test.ts
@@ -1,6 +1,6 @@
 import { assert, test } from "@hazae41/phobos";
 import { Buffer } from "node:buffer";
-import { ChaCha20Poly1305Cipher, load, Memory } from "./mod.ts";
+import { ChaCha20Cipher, ChaCha20Poly1305Cipher, load, Memory } from "./mod.ts";
 
 function equals(a: Uint8Array, b: Uint8Array) {
   return Buffer.from(a).equals(Buffer.from(b))
@@ -19,4 +19,26 @@ test("chacha20poly1305", async () => {
   using decrypted = chacha.decrypt(encrypted, nonce)
 
   assert(equals(message.bytes, decrypted.bytes))
+})
+
+
+test("chacha20", async () => {
+  await load()
+
+  using key = new Memory(crypto.getRandomValues(new Uint8Array(32)))
+  using nonce = new Memory(crypto.getRandomValues(new Uint8Array(12)))
+  using original = new Memory(crypto.getRandomValues(new Uint8Array(256)))
+  using modified = new Memory(original.bytes)
+
+  using encryptor = new ChaCha20Cipher(key, nonce)
+
+  encryptor.apply_keystream(modified)
+
+  assert(!equals(original.bytes, modified.bytes))
+
+  using decryptor = new ChaCha20Cipher(key, nonce)
+
+  decryptor.apply_keystream(modified)
+
+  assert(equals(original.bytes, modified.bytes))
 })
